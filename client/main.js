28f3560c36d4bc48cdb13340ff9b4d8df7aa8319
Gasper <duanpengyuan@qq.com>
2017-07-31 17:57:30 +0800

    scaffold

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import 'normalize.css'
+
+import { Meteor } from 'meteor/meteor'
+import React from 'react'
+import { render } from 'react-dom'
+import App from '/imports/App.jsx'
+
+Meteor.startup(() => render(<App />, document.getElementById('root')))
