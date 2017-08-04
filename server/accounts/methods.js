fd5f5f77b694036ca284db63b11630d815304049
GalinaGao123 <2901634865@qq.com>
2017-08-04 22:00:40 +0800

    coding

--- server/accounts/methods.js
+++ server/accounts/methods.js
@@ -1,8 +1,12 @@
 import { Meteor } from 'meteor/meteor'
 import { register } from './index.js'
+import { Accounts } from '/lib/collections.js'
 
 Meteor.methods({
   'accounts.register'(count) {
     Meteor.defer(() => register(count))
+  },
+  'accounts.update'(_id, type) {
+    Accounts.update({ _id }, { $set: { for: type } })
   }
 })
