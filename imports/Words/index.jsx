f10aeffc2a26ed156b9d74863245b7d896d539d0
Gasper <duanpengyuan@qq.com>
2017-08-11 15:37:24 +0800

    add tips

--- imports/Words/index.jsx
+++ imports/Words/index.jsx
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor'
 import React, { Component } from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
-import { Input, Button, Form, message } from 'antd'
+import { Input, Button, Form, message, Collapse } from 'antd'
 
 import { Words } from '/lib/collections.js'
 
@@ -36,11 +36,24 @@ class WordsComp extends Component {
 
     return (
       <Form onSubmit={this.handleSubmit}>
+        <Collapse style={{ marginBottom: 20 }}>
+          <Collapse.Panel key="1" header="模板用法">
+            <div>
+              {'{sym}'} 插入标点符号<br/>
+              {'{emoji}'} 插入 emoji 表情<br/>
+              {'{text}'} 插入字符表情<br/>
+            </div>
+          </Collapse.Panel>
+        </Collapse>
+
         <Form.Item>
           {form.getFieldDecorator('content', {
             initialValue: content
           })(
-            <Input.TextArea style={{ fontSize: 16 }} rows={30} disabled={!ready} />
+            <Input.TextArea
+              style={{ fontSize: 16 }}
+              autosize={{ minRows: 20, maxRows: 30 }}
+              disabled={!ready} />
           )}
         </Form.Item>
 
